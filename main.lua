8bd9c7fbcd96ded13c5de7dda03a37a4923e120a
drakewill-CRL <46307022+drakewill-CRL@users.noreply.github.com>
2020-07-28 14:58:55 -0400

    Significant improvements to the Proof of Concept

--- main.lua
+++ main.lua
@@ -3,10 +3,7 @@
 -----------------------------------------------------------------------------------------
 --remember, lua requires code to be in order to reference (cant call a function that's lower in the file than the current one)
 
---TODO: add an indicator to show facing. This should get updated on location change all the time.
---need a sprite to point a direction, and then rotate it.
-
---debugging helper functoin
+--debugging helper function
 function dump(o)
     if type(o) == 'table' then
        local s = '{ '
@@ -20,48 +17,42 @@ function dump(o)
     end
  end
 
--- Your code here
 require("database")
 local debug = true --set false for release builds.
 startDatabase()
 
 --uncomment when testing to clear local data.
 --ResetDatabase()
+--uncomment to add test cell in lower right corner of 11x11 grid
+--fillinTestCells()
 
 require("plusCodes")
 currentPlusCode = ""
 lastPlusCode = ""
 
+function fillinTestCells()
+    AddPlusCode("849VCRXR+4M") --5, -5 means lower right cell
+    --AddPlusCode("849VCRXR+9C") --home base cell, center
+end
 
 --TODO:
---draw a grid on screen. Unexplored tiles should be grey, explored tiles will be blue for now.
---rescale display? this game should be vertical.
 
+--display objects
 local locationText = display.newText("Current location:" .. currentPlusCode, display.contentCenterX, 400, native.systemFont, 16)
 local timeText = display.newText("Current time:" .. os.date("%X"), display.contentCenterX, 420, native.systemFont, 16)
 local countText = display.newText("Total Cells Explored: ?", display.contentCenterX, 440, native.systemFont, 16)
-
-
-
+local cellCollection = {}
 local gridGroup = display.newGroup()
-local unvisitedCell = {.3, .3, .3, 1}
-local visitedCell = {.1, .1, .7, 1}
-
-local rect11 = display.newRect(gridGroup, display.contentCenterX - 26, display.contentCenterY - 26, 25, 25) --x y w h
-local rect12 = display.newRect(gridGroup, display.contentCenterX, display.contentCenterY - 26, 25, 25) --x y w h
-local rect13 = display.newRect(gridGroup, display.contentCenterX + 26, display.contentCenterY - 26, 25, 25) --x y w h
-local rect21 = display.newRect(gridGroup, display.contentCenterX - 26, display.contentCenterY, 25, 25) --x y w h
-local rect22 = display.newRect(gridGroup, display.contentCenterX, display.contentCenterY, 25, 25) --x y w h
-local rect23 = display.newRect(gridGroup, display.contentCenterX + 26, display.contentCenterY, 25, 25) --x y w h
-local rect31 = display.newRect(gridGroup, display.contentCenterX - 26, display.contentCenterY + 26, 25, 25) --x y w h
-local rect32 = display.newRect(gridGroup, display.contentCenterX, display.contentCenterY + 26, 25, 25) --x y w h
-local rect33 = display.newRect(gridGroup, display.contentCenterX + 26, display.contentCenterY + 26, 25, 25) --x y w h
-
-local directionArrow = display.newImageRect(gridGroup, "arrow1.png", 25, 25)
+local directionArrow = display.newImageRect("arrow1.png", 25, 25)
 directionArrow.x = display.contentCenterX
 directionArrow.y = display.contentCenterY
 
+--color codes
+local unvisitedCell = {.3, .3, .3, 1}
+local visitedCell = {.1, .1, .7, 1}
 
+
+--this might be replaced by CreateCustomGrid, keeping for text display of grid values if I wanted to go down to level 11.
 function drawGrid()
 --currentPlusCode is in center of a 5x5 grid?
 --Need to work out which cells are around my cell? is there a way to math this, at least for a 10-digit code?
@@ -79,161 +70,60 @@ function drawGrid()
 --and J6, M6, Q6, and R6 in that column, and the combinations of those values on lat/long.
 --so J-R, 6-C. 25 total cells to look up when updating the grid. Can do.
 
-
 --rects per quadrant. Doing 3x3 for proof of concept, so 22 is the middle cell.
 --so, the coordinates are XY on the rect's name.
 
 
 end
 
-drawGrid()
-
-
+--replaced with UpdateCustomGrid?
 function paintGrid()
-    --this is where we set colors, call this function on locationChange
-    --PoC: color center rect.
-    --expand to 3x3 next. Last digit is X axis, second to last is Y axis
-
-    if (VisitedCell(currentPlusCode)) then
-        rect22.fill = visitedCell
-    else
-        rect22.fill = unvisitedCell
-    end
-
-    --center handled, now handle the surrounding cells.
-    local celltocheck = shiftCellEast(currentPlusCode)
-    if (debug) then print("East cell is " .. celltocheck) end
-    if (VisitedCell(celltocheck)) then
-        rect23.fill = visitedCell
-    else
-        rect23.fill = unvisitedCell
-    end
-    
-    celltocheck = shiftCellWest(currentPlusCode)
-    if (debug) then print("west cell is " .. celltocheck) end
-    if (VisitedCell(celltocheck)) then
-        rect21.fill = visitedCell
-    else
-        rect21.fill = unvisitedCell
-    end
-    
-    celltocheck = shiftCellNorth(currentPlusCode)
-    if (debug) then print("north cell is " .. celltocheck) end
-    if (VisitedCell(celltocheck)) then
-        rect12.fill = visitedCell
-    else
-        rect12.fill = unvisitedCell
-    end
-
-    celltocheck = shiftCellSouth(currentPlusCode)
-    if (debug) then print("south cell is " .. celltocheck) end
-    if (VisitedCell(celltocheck)) then
-        rect32.fill = visitedCell
-    else
-        rect32.fill = unvisitedCell
-    end
-
-    --and now 3x3 corners.
-    celltocheck = shiftCellNorth(shiftCellEast(currentPlusCode))
-    if (debug) then print("northeast cell is " .. celltocheck) end
-    if (VisitedCell(celltocheck)) then
-        rect13.fill = visitedCell
-    else
-        rect13.fill = unvisitedCell
-    end
-
-    celltocheck = shiftCellNorth(shiftCellWest(currentPlusCode))
-    if (debug) then print("northwest cell is " .. celltocheck) end
-    if (VisitedCell(celltocheck)) then
-        rect11.fill = visitedCell
-    else
-        rect11.fill = unvisitedCell
-    end
-
-    celltocheck = shiftCellSouth(shiftCellWest(currentPlusCode))
-    if (debug) then print("southwest cell is " .. celltocheck) end
-    if (VisitedCell(celltocheck)) then
-        rect31.fill = visitedCell
-    else
-        rect31.fill = unvisitedCell
-    end
-
-    celltocheck = shiftCellSouth(shiftCellEast(currentPlusCode))
-    if (debug) then print("southeast cell is " .. celltocheck) end
-    if (VisitedCell(celltocheck)) then
-        rect33.fill = visitedCell
-    else
-        rect33.fill = unvisitedCell
-    end
-
-end
-
-function shiftCellEast(pluscode)
-    --get last digit, move it 1 notch higher on the list
-    local currentDigit = pluscode:sub(11, 11)
-    local digitIndex = CODE_ALPHABET_:find(currentDigit)
-    digitIndex = digitIndex + 1
-    if (digitIndex == 21) then
-        --i probably also need to adjust position 8 in the string if this happens. Would be position 7 if north/south.
-        digitIndex = 1 
-    end
-    currentDigit = CODE_ALPHABET_:sub(digitIndex, digitIndex)
-    local newCode = pluscode:sub(1, 10) .. currentDigit
-    return newCode
-end
-
-function shiftCellWest(pluscode)
-    --get last digit, move it 1 notch higher on the list
-    local currentDigit = pluscode:sub(11, 11)
-    local digitIndex = CODE_ALPHABET_:find(currentDigit)
-    digitIndex = digitIndex - 1
-    if (digitIndex <= 0) then
-        --i probably also need to adjust position 8 in the string if this happens.
-        digitIndex = 20
-    end
-    currentDigit = CODE_ALPHABET_:sub(digitIndex, digitIndex)
-    local newCode = pluscode:sub(1, 10) .. currentDigit
-    return newCode
-end
-
-function shiftCellNorth(pluscode)
-    --get last digit, move it 1 notch higher on the list
-    local currentDigit = pluscode:sub(10, 10)
-    local digitIndex = CODE_ALPHABET_:find(currentDigit)
-    digitIndex = digitIndex + 1
-    if (digitIndex == 21) then
-        --i probably also need to adjust position 7 in the string if this happens.
-        digitIndex = 1 
-    end
-    currentDigit = CODE_ALPHABET_:sub(digitIndex, digitIndex)
-    local newCode = pluscode:sub(1, 9) .. currentDigit .. pluscode:sub(11,11)
-    return newCode
-end
-
-function shiftCellSouth(pluscode)
-    --get last digit, move it 1 notch higher on the list
-    local currentDigit = pluscode:sub(10, 10)
-    local digitIndex = CODE_ALPHABET_:find(currentDigit)
-    digitIndex = digitIndex - 1
-    if (digitIndex < 1) then
-        --i probably also need to adjust position 7 in the string if this happens.
-        digitIndex = 20 
-    end
-    currentDigit = CODE_ALPHABET_:sub(digitIndex, digitIndex)
-    local newCode = pluscode:sub(1, 9) .. currentDigit .. pluscode:sub(11,11)
-    return newCode
 end
 
 function shiftCell(pluscode, xShift, yShift)
     --take the current cell, move it some number of cells in both directions (positive or negative)
-    --will probably only work for values between -19 and 19. Shifting by 20 means you've move up 1 higher level cell entirely.
+    --will probably only work for values between -39 and 39. Shifting by 20 means you've move up 1 higher level cell entirely,
+    --and while this checks once for that, it doesn't check twice
     local newCode = pluscode
+    local currentDigit = ""
+    local digitIndex = 0
+    if (debug)then print ("shifting cell " .. pluscode) end
     --do X shift
     if (xShift ~= 0) then
+        if (debug)then print ("Shifting X " .. xShift) end
+        currentDigit = pluscode:sub(11, 11)
+        digitIndex = CODE_ALPHABET_:find(currentDigit)
+        digitIndex = digitIndex + xShift
+        --i probably also need to adjust position 8 in the string if this happens in either direction
+        if (digitIndex <= 0) then
+            digitIndex = 20 + digitIndex
+        end
+        if (digitIndex > 20) then
+            digitIndex = digitIndex - 20
+        end
+        currentDigit = CODE_ALPHABET_:sub(digitIndex, digitIndex)
+        newCode = newCode:sub(1, 10) .. currentDigit
     end
+    if (debug)then print ("newcode is " .. newCode) end
+
     --do Y shift
     if (yShift ~= 0) then
+        if (debug)then print ("shifting Y " .. yShift) end
+        --get last digit, move it 1 notch higher on the list
+        currentDigit = pluscode:sub(10, 10)
+        digitIndex = CODE_ALPHABET_:find(currentDigit)
+        digitIndex = digitIndex + yShift
+        --i probably also need to adjust position 7 in the string if this happens in either direction
+        if (digitIndex <= 0) then
+            digitIndex = 20 + digitIndex
+        end
+        if (digitIndex > 20) then
+            digitIndex = digitIndex - 20
+        end
+        currentDigit = CODE_ALPHABET_:sub(digitIndex, digitIndex)
+        newCode = newCode:sub(1, 9) .. currentDigit .. newCode:sub(11,11)
     end
+    if (debug)then print ("newcode is " .. newCode) end
 
     return newCode
 end
@@ -263,6 +153,8 @@ local function gpsListener(event)
     --todo update heading
     directionArrow.rotation = event.direction
 
+    UpdateCustomGrid() --this may need moved after changing plus codes
+
     if (lastPlusCode == currentPlusCode) then
         --dont update stuff, we're still standing in the same spot.
         return
@@ -276,12 +168,42 @@ local function gpsListener(event)
 
     --do screen updates
     paintGrid()
-
     
     locationText.text = "Current location:" .. currentPlusCode
     countText.text = "Total Explored Cells: " .. TotalExploredCells()
 
-    
+end
+
+function CreateSquareGrid(gridSize)
+    --instead of hard-coding the grid, how do i dynamically make it?
+    --size is square, X by Y size. Must be odd so that i can have a center square.
+    local padding = 1 --space between cells.
+    local cellSize = 25 -- square size in pixels
+    local range = math.floor(gridSize / 2) -- 7 becomes 3, which is right.
+
+    for x = -range, range, 1 do
+        for y = -range, range, 1 do
+            --create cell, tag it with x and y values.
+            local newSquare = display.newRect(gridGroup, display.contentCenterX + (cellSize * x) + x , display.contentCenterY + (cellSize * y) + y , cellSize, cellSize) --x y w h
+            newSquare.gridX = x
+            newSquare.gridY = -y --invert this so cells get identified top-to-bottom, rather than bottom-to-top
+            cellCollection[#cellCollection + 1] = newSquare
+        end
+    end
+end
+
+CreateSquareGrid(11)
+
+function UpdateCustomGrid()
+    for square = 1, #cellCollection do --this is supposed to be faster than ipairs
+        if (debug) then print("displaycell " .. cellCollection[square].gridX .. "," .. cellCollection[square].gridY) end
+        --check each spot based on current cell, modified by gridX and gridY
+        if VisitedCell(shiftCell(currentPlusCode, cellCollection[square].gridX, cellCollection[square].gridY)) then
+            cellCollection[square].fill = visitedCell
+        else
+            cellCollection[square].fill = unvisitedCell
+        end
+    end
 end
 
 --will need to remove this manually on exit
