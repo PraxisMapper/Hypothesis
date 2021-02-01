3933d8564674284358458f3b8c1dc8d8e9c97f17
drakewill-CRL <46307022+drakewill-CRL@users.noreply.github.com>
2021-01-31 23:21:08 -0500

    First major commit for going public cleanup.

--- config.lua
+++ config.lua
@@ -20,11 +20,4 @@ application =
 		},
 		--]]
 	},
-	license =
-    {
-        google =
-        {
-            key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAifzatodKYdPLYlvXj3Wy2sY5qUcWxtUNKID6Lyu0zUUWCKA/Ks9FXhEizTwjoKZb/4wrD7XIkRoA7jIkkQ+IShncBNhfaXE7MgSC+mIanN1Clf66pbIRZwTd5QmYJ/CtJq0Y8apy1kHzs0Nxbfbo8qBXHrAw3ERSsO0qGILbHkHF4UcYf2/4LiepejGlTduN81vlQk0JqWosqf7JujAi0cJjYiTXn+2Lrj4CiErXqRJo01EtidhJyOZeUfOl2bfrZkZcMnNVySYQR5o70BN6Y+9v3xUoT2iZDSt/XXtztlDzJLsXOenp/GqM3gZklKszE71HI393ZXRo+zitY1x4uwIDAQAB",
-        },
-    },
 }
