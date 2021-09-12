29acf0406c666d282052eb30df55ffc497d74e58
drakewill-CRL <46307022+drakewill-CRL@users.noreply.github.com>
2021-09-12 10:48:48 -0400

    Delete localNetwork.lua

--- localNetwork.lua
+++
@@ -1,8 +0,0 @@
--- -- the class for handling sending data to/from the API server
--- -- NOTE: naming this 'network' overrides the internal Solar2d library with the same name and breaks everything.
--- local composer = require("composer")
--- require("database")
--- require("helpers") --for Split
-
--- serverURL = ""
-
